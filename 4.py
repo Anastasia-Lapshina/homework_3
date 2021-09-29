d4b27ef06736e403152c4cf567724e4b4ad4d67e
Anastasia Lapshina <anapluslap@gmail.com>
2021-09-30 01:44:35 +0300

    Added formated strings

--- 4.py
+++ 4.py
@@ -1,7 +1,9 @@
 years = int(input("Enter the number of years you have "))
 minutes = years * 525600 * 1/3
 num = 2800000 * minutes / (2800000 * 5)
-print("You'll be able to see " + str(num) + " exhibits")
+msg = f'You will be able to see {num} exhibits'
+print(msg)
 exh = int(input("Enter the number of exhibits you would like to see "))
 time = exh * 5
-print("The amount of time you will need to do so equals " + str(time) + " minutes")
+message = f'The amount of time you will need to do so equals {time} minutes'
+print(message)
