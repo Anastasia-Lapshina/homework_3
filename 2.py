f138b9d8f5bc6ef4d647e4349653c093696efda7
Anastasia Lapshina <anapluslap@gmail.com>
2021-10-02 09:40:53 +0300

    fixed the formula calculating a hypotenuse

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 a = float(input("Enter the first leg "))
 b = float(input("Enter the second leg "))
-c = a ** 2 + b ** 2
+c = (a ** 2 + b ** 2) ** 0.5
 P = a + b + c
 S = 1/2 * a * b
 print("The perimeter of the triangle is " + str(P))
